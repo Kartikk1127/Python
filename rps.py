a69d1ba80b6a8dbc11e1074793805c8a2f18ef3a
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2024-02-01 07:29:40 +0530

    taking inputs

---
+++ rps.py
@@ -0,0 +1,10 @@
+import sys
+
+print("")
+
+playerchoice = input('Enter...\n1 for Rock,\n2 for Paper, or \n3 for Scissors:\n\n')
+
+player = int(playerchoice)
+
+if player < 1 or player > 3:
+    sys.exit("You must enter 1,2 or 3")
